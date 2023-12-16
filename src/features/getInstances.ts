6dae67d604e567a9f3dd4c06139bb491e61df8a8
minoshintaro <minos@concentinc.jp>
2023-12-16 13:27:08 +0900

    edit

--- src/features/getInstances.ts
+++ src/features/getInstances.ts
@@ -1,23 +1,24 @@
 export function getInstances(): InstanceNode[] {
-  const isTarget = (node: SceneNode): node is InstanceNode => {
-    if (!node.visible || node.type !== 'INSTANCE') return false;
 
-    let parent = node.parent;
+  const instances = figma.currentPage.findAllWithCriteria({
+    types: ['INSTANCE']
+  });
+
+  const filteredInstances = instances.filter(instance => {
+    let parent = instance.parent;
     while (parent) {
       if (parent.type === 'COMPONENT' || parent.type === 'INSTANCE') return false;
       parent = parent.parent;
     }
-
     return true;
-  };
+  });
 
-  const instances = figma.currentPage.children.filter(isTarget); // as InstanceNode[]
-  const sortedInstances = instances.sort((a, b) => {
+  const sortedInstances = filteredInstances.sort((a, b) => {
     const nameA = a.mainComponent ? a.mainComponent.name.toUpperCase() : "";
     const nameB = b.mainComponent ? b.mainComponent.name.toUpperCase() : "";
     return nameA.localeCompare(nameB);
   });
-  // console.log('test', instances, sortedInstances);
+  console.log('test', instances, sortedInstances);
 
-  return instances;
+  return sortedInstances;
 }
