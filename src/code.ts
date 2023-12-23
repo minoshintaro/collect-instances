1dbf192ab88f4b73bb72aad80c21df8a30f5fa28
minoshintaro <minos@concentinc.jp>
2023-12-23 15:54:08 +0900

    edit

--- src/code.ts
+++ src/code.ts
@@ -16,6 +16,7 @@ figma.skipInvisibleInstanceChildren = true;
 
 figma.on('run', async () => {
   // [0] 事前処理
+  figma.notify('Doing...');
   await figma.loadFontAsync(FONT_NAME);
 
   // [1] 配置先の生成
@@ -32,7 +33,7 @@ figma.on('run', async () => {
   // [2] インスタンスの収集
   const collectionMap = generateInstanceMap(figma.currentPage.children);
 
-  // [3] 処理
+  // [3] 繰り返し処理
   for (const collection of collectionMap) {
     const masterComponent = collection[0];
     const instances = collection[1]
@@ -66,6 +67,11 @@ figma.on('run', async () => {
     // [3-3] 配置
     layoutFrame.appendChild(stackFrame);
   }
+
+  const sorted = [...layoutFrame.children].sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  sorted.forEach(frame => layoutFrame.appendChild(frame));
 
   // [4] 当該ページを表示
   figma.currentPage = targetPage;
